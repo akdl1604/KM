c72d556a16815a8c45c1fa776847bc8e5131631f
Hyunbinpark <wisdom3227@gmail.com>
2021-11-03 16:03:49 +0900

    Client tcp/ip

--- Client/Client/ClientDlg.cpp
+++ Client/Client/ClientDlg.cpp
@@ -6,16 +6,44 @@
 #include "framework.h"
 #include "Client.h"
 #include "ClientDlg.h"
+#include "ConnectSocket.h"
 #include "afxdialogex.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+	// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
 
 // CClientDlg 대화 상자
-
-
 
 CClientDlg::CClientDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_CLIENT_DIALOG, pParent)
@@ -26,9 +54,11 @@ CClientDlg::CClientDlg(CWnd* pParent /*=nullptr*/)
 void CClientDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+
 }
 
 BEGIN_MESSAGE_MAP(CClientDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
 END_MESSAGE_MAP()
@@ -40,14 +70,38 @@ BOOL CClientDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+		}
+	}
+
 	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
 	//  프레임워크가 이 작업을 자동으로 수행합니다.
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
-
+	m_Socket.Create();
+	if (m_Socket.Connect(L"127.0.0.1", 9000) == FALSE)
+	{
+		AfxMessageBox(L"ERROR: Failed to connect server");
+		PostQuitMessage(0);
+		return FALSE;
+	}
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CClientDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+
 }
 
 // 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
---
+++ Client/Client/ConnectSocket.cpp
@@ -0,0 +1,46 @@
+﻿// ConnectSocket.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Client.h"
+#include "ClientDlg.h"
+#include "ConnectSocket.h"
+
+
+// CConnectSocket
+
+CConnectSocket::CConnectSocket()
+{
+}
+
+CConnectSocket::~CConnectSocket()
+{
+}
+
+
+// CConnectSocket 멤버 함수
+
+void CConnectSocket::OnClose(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+
+	CSocket::OnClose(nErrorCode);
+	AfxMessageBox(L"ERROR: Disconnected from server!");
+	::PostQuitMessage(0);
+	ShutDown();
+	Close();
+}
+
+
+void CConnectSocket::OnReceive(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	TCHAR szBuffer[1024];
+	::ZeroMemory(szBuffer, sizeof(szBuffer));
+
+	if (Receive(szBuffer, sizeof(szBuffer)) > 0)
+	{
+		CClientDlg* pMain = (CClientDlg*)AfxGetMainWnd();
+	}
+	CSocket::OnReceive(nErrorCode);
+}
