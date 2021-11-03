ef34c5e9df06d193d99186ac917deb7949cc883f
kangjaehyeon526 <rlfrlf526@naver.com>
2021-11-03 16:00:49 +0900

    server tcp/ip

--- Server/Server/ServerDlg.h
+++ Server/Server/ServerDlg.h
@@ -9,8 +9,11 @@
 class CServerDlg : public CDialogEx
 {
 // 생성입니다.
+private:
+	ServerSock* pServer;
 public:
 	CServerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	~CServerDlg();
 
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
@@ -24,10 +27,13 @@ public:
 // 구현입니다.
 protected:
 	HICON m_hIcon;
-
 	// 생성된 메시지 맵 함수
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_ctrlListSock;
+	CListBox m_ctrlListResult;
+	afx_msg void OnTcnSelchangeTab1(NMHDR* pNMHDR, LRESULT* pResult);
 };
--- Server/Server/resource.h
+++ Server/Server/resource.h
@@ -2,8 +2,9 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // Server.rc에서 사용되고 있습니다.
 //
-#define IDD_SERVER_DIALOG               102
 #define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     130
+#define IDD_SERVER_DIALOG               130
 #define IDC_LIST1                       1000
 #define IDC_TAB1                        1001
 #define IDC_LIST2                       1002
@@ -20,7 +21,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
